5e11066f610b34060fd10fe998c55c69b39b7dae
Juan Ribeiro <juan.ribeiro3@outlook.com>
2022-11-03 21:30:33 -0300

    Requisito 5 finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,19 +4,63 @@ import Carregando from './Carregando';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputName: '',
+      isInputValid: false,
+    };
+  }
+
   componentDidMount() {
     const { handleCarregando } = this.props;
     handleCarregando();
   }
 
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.validateInputUserName);
+  };
+
+  validateInputUserName = () => {
+    const { inputName } = this.state;
+    const numberValidate = 2;
+    const nameLength = inputName.length >= numberValidate;
+    this.setState({ isInputValid: nameLength });
+  };
+
   render() {
     const { carregando } = this.props;
+    const { inputName, isInputValid } = this.state;
 
     if (carregando) return <Carregando />;
 
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="nome-banda">
+            <input
+              type="text"
+              name="inputName"
+              value={ inputName }
+              onChange={ this.handleChange }
+              id="nome-banda"
+              data-testid="search-artist-input"
+              placeholder="Nome do artista ou banda "
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ !isInputValid }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
